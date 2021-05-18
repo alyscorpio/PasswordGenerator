a3b7f6ac6ab83edc26e69e4ebbb0c0b6717d13e8
Alys Scorpio <alyssad1182@gmail.com>
2021-05-18 16:46:52 -0700

    writing out game plan for java

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-// Assignment Code
+// select button in html with the id "generate" and assign it to the variable generateBtn
 var generateBtn = document.querySelector("#generate");
+
+// create arrays for character groups (uppercase, lowercase, numbers, special characters)
+
+// LENGTH OF PASSWORD
+// create function to generate password between 8 and 128 characters
+
+// PASSWORD CRITERIA PROMPTS
+// prompt user about password criteria (length, uppercase, lowercase, numbers, special characters)
+
+// GENERATING PASSWORD
+// function to generate random password based on criteria (if, then)
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +23,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
