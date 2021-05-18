fc056c85c23aab3c9bbc76de7e429c9dd9018a89
Alys Scorpio <alyssad1182@gmail.com>
2021-05-18 16:57:41 -0700

    adding arrays for character types

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // create arrays for character groups (uppercase, lowercase, numbers, special characters)
+var bigCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var littleCharacters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var specialCharacters = ["!", "@", "#", "$", "&", "%", "?", "*"]
 
 // LENGTH OF PASSWORD
 // create function to generate password between 8 and 128 characters
