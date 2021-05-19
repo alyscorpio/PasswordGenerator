eff3c82049a707fa4d99d31b9b282d28182141ba
Alys Scorpio <alyssad1182@gmail.com>
2021-05-18 18:44:51 -0700

    adding to password parameters, adding if statements.

--- script.js
+++ script.js
@@ -12,11 +12,42 @@ console.log(littleCharacters);
 console.log(numberCharacters);
 console.log(specialCharacters);
 
+// click function for generate button
+generateBtn.addEventListener("click", function enterInfo() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+// when button is clicked, user is prompted for password parameters
+
 // LENGTH OF PASSWORD
 // create function to generate password between 8 and 128 characters
+var chooseLength = window.prompt("How long do you want your password to be? (Pick a number between 8 & 128 to ensure maximum safety!)");
+  if (userLength < 8) {
+    window.alert("Oh no, that password length is too short! Please choose a number between 8 & 128.");
+    return;
+  }
+
+  // if user input is > 128, reject
+  if (userLength > 128) {
+    window.alert("Uh oh! Your password is too long. Please try again.");
+    return;
+  }
 
 // PASSWORD CRITERIA PROMPTS
 // prompt user about password criteria (length, uppercase, lowercase, numbers, special characters)
+window.prompt ("First, give me some guidelines for your password.")
+
+// When the user clicks on <div>, open the popup
+function 
+
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
 
 // GENERATING PASSWORD
 // function to generate random password based on criteria (if, then)
