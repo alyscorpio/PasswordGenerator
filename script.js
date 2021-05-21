581c6b377e26694c6a76ce535d8e330e3bfff673
Alys Scorpio <alyssad1182@gmail.com>
2021-05-21 10:49:01 -0700

    setting the functions in java to test password parameters

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var generateBtn = document.querySelector('#generate');
 
+// Create arrays for character groups (uppercase, lowercase, numbers, special characters)
 var bigCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var littleCharacters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
@@ -12,78 +13,52 @@ console.log(littleCharacters);
 console.log(numberCharacters);
 console.log(specialCharacters);
 
-var generateBtn = document.querySelector('#generate');
-generateBtn.addEventListener('click', formPop);
-
-function formPop() {
-  var popup = document.getElementById('myForm');
-  popup.classList.toggle('show');
-}
-
-var sumbitBtn = document.querySelector('#submit');
-sumbitBtn.addEventListener('click', passCriteria);
-
-var passLength = document.querySelector('#p-length');
-
 function generatePassword()
 
-// if (passLength < 8) {
-  //       window.alert("Sorry, that's too short. Passwords must be at least 8 characters to ensure your safety!");
-  //       return;
-  //     }
-  //     if (passLength > 128) {
-  //       window.alert("Woah, that's a little too long! Please pick a length under 128 characters.");
-  //       return;
-  //     }
+// LENGTH
+var passLength = window.prompt("How many characters do you want?");
+  if (passLength < 8) {
+    window.alert("Too short! Are you trying to get hacked?! Password lengths have to be at least 8 characters, DUH.");
+    return;
+  }
+  if (passLength > 128) {
+    window.alert("Wayyyyy too long! You think you could even remember all of that? Pick a length under 128 characters.");
+    return;
+  }
 
-//   var passCap = document.getElementById('myform').elements['cap'].value;
-//     if (passCap === 'true') {
-//       finalArray = finalArray.concat(bigCharacters);
-//     }
+// CAPITAL LETTERS
+var passCap = window.confirm("Uppercase?");
 
-//   var passLittle = document.getElementById('myform').elements['little'].value;
-//     if (passLittle === 'true') {
-//       finalArray = finalArray.concat(littleCharacters);
-//     }
-  
-//   var passNumb = document.getElementById('myform').elements['numb'].value;
-//     if (passNumb === 'true') {
-//       finalArray = finalArray.concat(numberCharacters);
-//     }
+if (passCap === true) {
+  finalArray = finalArray.concat(bigCharacters);
+}
 
-//   var passSpec = document.getElementbyId('myform').elements['spec'].value;
-//     if (passSpec === 'true') {
-//       finalArray = finalArray.concat(specialCharacters);
-//     }
+// LOWERCASE LETTERS
+var passNoCap = window.confirm("Lowercase characters?");
+console.log(passCap);
 
-//   console.log(finalArray)
+if (passNoCap === true) {
+  finalArray = finalArray.concat(littleCharacters);
+}
 
-// }
+// NUMBERS
+var passNumb = window.confirm("Numbers?");
+console.log(passNumb);
 
+if (passNumb === true) {
+  finalArray = finalArray.concat(numberCharacters);
+}
 
+// SPECIAL CHARACTERS
+var passSpec = window.confirm("Special characters?");
+console.log(passSpec);
 
+if (passSpec === true) {
+  finalArray = finalArray.concat(specialCharacters);
+}
 
-// var generateBtn = document.querySelector('#generate');
-// var formPop = document.querySelector('#form')
+console.log(finalArray);
 
-// generateBtn.addEventListener('click', popup);
-
-// function popup () {
-//   document.getElementById('form').style.display=('show');
-// };
-
-// function popup() {
-//   popup.classList.toggle("show");
-// }
-
-// on click make form popup
-
-// console log data from form
-
-//
-
-// select button in html with the id "generate" and assign it to the variable generateBtn
-// var generateBtn = document.querySelector("#generate");
 
 // // create arrays for character groups (uppercase, lowercase, numbers, special characters)
 // var bigCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
