58a15b2d9b6d969e1d75c2e88c9c84d49eeeb3e8
Alys Scorpio <alyssad1182@gmail.com>
2021-05-21 11:49:12 -0700

    improving function so that it returns a random password according to the parameters

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var generateBtn = document.querySelector('#generate');
 
 // Create arrays for character groups (uppercase, lowercase, numbers, special characters)
@@ -5,160 +6,85 @@ var bigCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 var littleCharacters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 var specialCharacters = ["!", "@", "#", "$", "&", "%", "?", "*"];
-var passlength = '';
 var finalArray = [];
 
-console.log(bigCharacters);
-console.log(littleCharacters);
-console.log(numberCharacters);
-console.log(specialCharacters);
+  console.log(bigCharacters);
+  console.log(littleCharacters);
+  console.log(numberCharacters);
+  console.log(specialCharacters);
 
 function generatePassword() {
 
-  // LENGTH
-  var passLength = window.prompt("How many characters do you want?");
-    if (passLength < 8) {
-      window.alert("Too short! Are you trying to get hacked?! Password lengths have to be at least 8 characters, DUH.");
-      return;
-    }
-    if (passLength > 128) {
-      window.alert("Wayyyyy too long! You think you could even remember all of that? Pick a length under 128 characters.");
-      return;
-    }
+// LENGTH
+var passLength = window.prompt("How many characters do you want?");
+  if (passLength < 8) {
+    window.alert("Too short! Are you trying to get hacked?! Password lengths have to be at least 8 characters, DUH.");
+    return;
+  }
+  if (passLength > 128) {
+    window.alert("Wayyyyy too long! You think you could even remember all of that? Pick a length under 128 characters.");
+    return;
+  }
 
-  // CAPITAL LETTERS
-  var passCap = window.confirm("Uppercase?");
+// CAPITAL LETTERS
+var passCap = window.confirm("Uppercase?");
 
+// LOWERCASE LETTERS
+var passNoCap = window.confirm("Lowercase characters?");
+console.log(passCap);
+
+// NUMBERS
+var passNumb = window.confirm("Numbers?");
+console.log(passNumb);
+
+// SPECIAL CHARACTERS
+var passSpec = window.confirm("Special characters?");
+console.log(passSpec);
+
+  // Include capital letters if user approves
   if (passCap === true) {
     finalArray = finalArray.concat(bigCharacters);
+    console.log(finalArray);
   }
 
-  // LOWERCASE LETTERS
-  var passNoCap = window.confirm("Lowercase characters?");
-  console.log(passCap);
-
+  // Include lowercase letters if user approves
   if (passNoCap === true) {
     finalArray = finalArray.concat(littleCharacters);
+    console.log(finalArray);
   }
 
-  // NUMBERS
-  var passNumb = window.confirm("Numbers?");
-  console.log(passNumb);
-
+  // Include numbers if user approves
   if (passNumb === true) {
     finalArray = finalArray.concat(numberCharacters);
+    console.log(finalArray);
   }
 
-  // SPECIAL CHARACTERS
-  var passSpec = window.confirm("Special characters?");
-  console.log(passSpec);
-
+  // Include special characters if user approves
   if (passSpec === true) {
     finalArray = finalArray.concat(specialCharacters);
+    console.log(finalArray);
   }
 
-  console.log(finalArray);
+console.log(finalArray);
 
-  // Final password is an empty string
+// Final password is an empty string
   var finalPass = "";
 
-  // For loop to generate a random set of characters from the finalArray, the length of passLength
-  for (var i = 0; i< userLength; i++) {
-    finalPass = finalPass + finalarray[Math.floor(Math.random() * finalArray.length)];
+// For loop to generate a random set of characters from the finalArray, the length of passLength
+  for (var i = 0; i< passLength; i++) {
+    finalPass = finalPass + finalArray[Math.floor(Math.random() * finalArray.length)];
   }
   console.log(finalPass);
 
-  return finalPass;
+return finalPass;
 }
 
+function writePassword () {
+  var finalPass = generatePassword();
+  var passText = document.querySelector('#password');
 
+  passText.value = password;
 
+}
 
-
-// // create arrays for character groups (uppercase, lowercase, numbers, special characters)
-// var bigCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// var littleCharacters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
-// var specialCharacters = ["!", "@", "#", "$", "&", "%", "?", "*"];
-// var finalArray = [];
-
-// console.log(bigCharacters);
-// console.log(littleCharacters);
-// console.log(numberCharacters);
-// console.log(specialCharacters);
-
-// PASSWORD CRITERIA
-// prompt user about password length
-// generateBtn.addEventListener("click", passPrompt, generatePassword, writePassword);
-
-// function passPrompt() {
-
-//   var passLength = window.prompt("How long should the password be?");
-//     console.log(passLength);
-//     // set password parameters between 8 and 128 characters
-//     if (passLength < 8) {
-//       window.alert("Sorry, that's too short. Passwords must be at least 8 characters to ensure your safety!");
-//       return;
-//     }
-//     if (passLength > 128) {
-//       window.alert("Woah, that's a little too long! Please pick a length under 128 characters.");
-//       return;
-//     }
-  
-//   var passCap = window.confirm("Lowercase characters?");
-//   console.log(passCap);
-
-//   // include uppercase characters if user selects okay
-//   if (passCap === true) {
-//     finalArray = finalArray.concat(bigCharacters);
-//   }
-
-//   var passNoCap = window.confirm("Uppercase?");
-//   console.log(passNoCap);
-
-//   // include lowercase characters if user selects okay
-//   if (passNoCap === true) {
-//     finalArray = finalArray.concat(littleCharacters);
-//   }
-    
-//   var passNumb = window.confirm("Numbers?");
-//   console.log(passNumb);
-
-//   // include numbers if user selects okay
-//   if (passNumb === true) {
-//     finalArray = finalArray.concat(numberCharacters);
-//   }
-
-//   var passSpec = window.confirm("Special characters?");
-//   console.log(passSpec);
-
-//   // include special characters if user selects okay
-//   if (passSpec === true) {
-//     finalArray = finalArray.concat(specialCharacters);
-//   }
-
-//   console.log(finalArray);
-// }
-
-// // GENERATING PASSWORD
-// var password = "";
-
-// function generatePassword() {
-//   for (i = 0; i < passLength; i++) {
-//     password = password + finalArray[Math.floor(Math.random() * finalArray.length)];
-//   }
-
-// console.log(password);
-// // // for (var i = 0; i < passLength; i++) {
-// // //   password = password + finalArray[Math.floor(Math.random() * finalArray.length)];
-// // // }
-
-// // console.log(password);
-
-// // // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-//   password.writePassword ();
-// }
+generateBtn.addEventListener('click', writePassword);
